4f5bb7fb83dfe30a7e4c0857bff32a056207faed
Ash Lai <z0975201508@gmail.com>
2021-08-14 15:39:06 +0800

    add navbar style sheet include logo、links、hamburger icon and rwd settings

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -12,11 +12,13 @@ const Navbar = () => {
       {/* links */}
       <ul className="navbar__links">
         <li>
-          <Link to="/cart">
+          <Link to="/cart" className="cart__link">
             {/* Icon */}
             <i className="fas fa-shopping-cart"></i>
-            Cart
-            <span className="cartlogo__badge">0</span>
+            <span>
+              Cart
+              <span className="cartlogo__badge">0</span>
+            </span>
           </Link>
         </li>
         <li>
