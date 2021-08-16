34cadd3b4e675bc70da5ad445cdf901fde066d6f
Ash Lai <z0975201508@gmail.com>
2021-08-16 15:51:37 +0800

    Create ProductItem Component in Cart Page

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,7 +1,30 @@
 import './CartPage.scss';
 
+// Components
+import CartItem from '../components/CartItem';
+
 const CartPage = () => {
-  return <div className="cartpage">CartPage</div>;
+  return (
+    <div className="cartpage">
+      <div className="cartpage__left">
+        <h2>Shopping Cart</h2>
+
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+      <div className="cartpage__right">
+        <div className="cartpage__info">
+          <p>Subtotal (0) items</p>
+          <p>$499.99</p>
+        </div>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
