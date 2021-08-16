4fbcb405bff43b7ca19ac9cf62218ea74362ea06
Ash Lai <z0975201508@gmail.com>
2021-08-16 15:17:29 +0800

    add fake data and style sheet for ProductPage component

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,7 +1,48 @@
 import './ProductPage.scss';
 
 const ProductPage = () => {
-  return <div className="productpage">ProductPage</div>;
+  return (
+    <div className="productpage">
+      <div className="productpage__left">
+        <div className="left__image">
+          <img
+            src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+            alt="product name"
+          />
+        </div>
+        <div className="left__info">
+          <p className="left__name">Product 1</p>
+          <p className="left__price">Price: $499.99</p>
+          <p>
+            Description: Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Alias eos, esse blanditiis numquam vel porro!
+          </p>
+        </div>
+      </div>
+      <div className="productpage__right">
+        <div className="right__info">
+          <p>
+            Price: <span>$499.99</span>
+          </p>
+          <p>
+            Status: <span>In Stock</span>
+          </p>
+          <p>
+            Qty
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </p>
+          <p>
+            <button type="button">Add To Cart</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
