9dbafc8f0553c331586fd1dfbb3c434ff5ded1a0
Ash Lai <z0975201508@gmail.com>
2021-08-17 11:41:24 +0800

    use useEffect、useDispatch、useSelector to call the mongoDB's data from backend

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,23 @@
 import './HomePage.scss';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// Components
 import Product from '../components/Product';
 
+// Actions
+import { getProducts as listProducts } from '../redux/actions/productActions';
+
 const HomePage = () => {
+  const dispatch = useDispatch();
+
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <div className="homepage">
       <h2 className="homepage__title">Latest Products</h2>
