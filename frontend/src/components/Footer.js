e25ab62014e5be56cbcc8cf844726a9340faa0c6
Ash Lai <z0975201508@gmail.com>
2021-11-10 15:51:35 +0800

    fix: adjust footer chinese to english

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <div className='footer'>
-      <div>© 2021 Ash Lai. 版權所有 </div>
+      <div>© 2021 Ash Lai. Copyrights </div>
     </div>
   );
 };
